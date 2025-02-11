201e50aedb1eaffe8b1629abb0607aec56b47851
bannmoore <moore.brittanyann@gmail.com>
2025-02-10 16:41:44 -0800

    Better registry access token management.

--- bin/deploy_application.sh
+++ bin/deploy_application.sh
@@ -9,7 +9,9 @@ IFS=$'\n\t'
 CONTAINER_REGISTRY=bam # update this to your registry
 
 doctl auth init
-doctl registry login
+doctl registry login --expiry-seconds 86400
 
 docker compose build app
 docker push registry.digitalocean.com/$CONTAINER_REGISTRY/super-duper-app
+
+doctl registry logout
